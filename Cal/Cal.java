8eb933a48db8d6b5f435d0044ea75ac54032964a
Wang <ppiswinner@gmail.com>
2019-06-17 12:59:38 +0900

    Add files via upload

---
+++ Cal/Cal.java
@@ -0,0 +1,53 @@
+import java.util.Stack;
+
+public class Cal {
+
+	public static void main(String[] args) {
+
+		String[] srcList = { "1", "+", "2", "*", "3", "/", "40", "/", "2", "+", "2", "*", "3" };
+
+		Stack<String> stack = new Stack<String>();
+
+		for (int i = 0; i < srcList.length; i++) {
+			String cell = srcList[i];
+			double item;
+			switch (cell) {
+			case "*":
+				item = Double.valueOf(stack.pop()) * Double.valueOf(srcList[1 + i++]);
+				stack.push(String.valueOf(item));
+				break;
+			case "/":
+				item = Double.valueOf(stack.pop()) / Double.valueOf(srcList[1 + i++]);
+				stack.push(String.valueOf(item));
+				break;
+			default:
+				stack.push(cell);
+				break;
+			}
+		}
+
+		while (stack.size() > 1) {
+			double num1 = Double.valueOf(stack.pop());
+			String op = stack.pop();
+			double num2 = Double.valueOf(stack.pop());
+			double item;
+			switch (op) {
+			case "+":
+				item = num2 + num1;
+				stack.push(String.valueOf(item));
+				break;
+			case "-":
+				item = num2 - num1;
+				stack.push(String.valueOf(item));
+				break;
+			default:
+
+				break;
+			}
+		}
+
+		System.out.println(stack.toString());
+
+	}
+
+}
